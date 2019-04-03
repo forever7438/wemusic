27931f1df0d730dd9d9ee8d176bb35365861fc31
forget71 <743867239@qq.com>
2019-04-03 22:32:07 +0800

    组件修改

--- pages/myCoupon/myCoupon.vue
+++ pages/myCoupon/myCoupon.vue
@@ -1,19 +1,22 @@
 <template>
 	<view>
+		<view>
+			text
+		</view>
 		<couponList></couponList>
 	</view>
 </template>
 
 <script>
-import couponList from '../../components/item/couponList.vue';
-export default {
-	components:{
-		couponList
-	},
-	data() {
-		return {};
-	}
-};
+	import couponList from '../../components/item/couponList.vue';
+	export default {
+		components: {
+			couponList
+		},
+		data() {
+			return {};
+		}
+	};
 </script>
 
 <style lang="less"></style>
--- pages/tabbar/tabbar-5/tabbar-5.vue
+++ pages/tabbar/tabbar-5/tabbar-5.vue
@@ -80,149 +80,170 @@
 </template>
 
 <script>
-export default {
-	data() {
-		return {
-			title: 'Hello'
-		};
-	},
-	onLoad() {},
-	methods: {}
-};
+	export default {
+		data() {
+			return {
+				title: 'Hello'
+			};
+		},
+		onLoad() {},
+		methods: {}
+	};
 </script>
 
 <style lang="less">
-.content {
-	text-align: center;
-	height: 400upx;
-	padding: 0 15upx;
-	.messgae {
-		height: 146 * 2upx;
-		padding: 30upx;
-		background: linear-gradient(135deg, rgba(217, 179, 121, 1) 0%, rgba(162, 127, 74, 1) 100%);
-		border-radius: 16upx;
-		.user_info {
-			display: flex;
-			align-items: center;
-			image {
-				width: 92upx;
-				height: 92upx;
-				border: 4upx solid #fff;
-				border-radius: 50%;
-			}
-			view {
+	.content {
+		text-align: center;
+		height: 400upx;
+		padding: 0 15upx;
+
+		.messgae {
+			height: 146 * 2upx;
+			padding: 30upx;
+			background: linear-gradient(135deg, rgba(217, 179, 121, 1) 0%, rgba(162, 127, 74, 1) 100%);
+			border-radius: 16upx;
+
+			.user_info {
 				display: flex;
-				flex-direction: column;
-				text-align: left;
-				margin-left: 10upx;
-				.user_name {
-					font-size: 36upx;
-					font-family: PingFangSC-Medium;
-					font-weight: 600;
-					color: rgba(255, 230, 190, 1);
-					::after {
-						display: inline-block;
-						width: 10upx;
-						height: 10upx;
-						content: '>';
-						margin-left: 20upx;
+				align-items: center;
+
+				image {
+					width: 92upx;
+					height: 92upx;
+					border: 4upx solid #fff;
+					border-radius: 50%;
+				}
+
+				view {
+					display: flex;
+					flex-direction: column;
+					text-align: left;
+					margin-left: 10upx;
+
+					.user_name {
+						font-size: 36upx;
+						font-family: PingFangSC-Medium;
+						font-weight: 600;
+						color: rgba(255, 230, 190, 1);
+
+						::after {
+							display: inline-block;
+							width: 10upx;
+							height: 10upx;
+							content: '>';
+							margin-left: 20upx;
+						}
 					}
-				}
-				.user_type {
-					font-size: 20upx;
-					font-family: PingFangSC-Regular;
-					font-weight: 400;
-					color: rgba(255, 230, 190, 1);
-				}
-			}
-		}
-		.user_money {
-			display: flex;
-			align-items: flex-end;
-			justify-content: space-between;
-			.money_info {
-				display: flex;
-				flex-direction: column;
-				text-align: left;
-				margin-top: 40upx;
-				text {
-					&:nth-of-type(1) {
-						width: 100upx;
-						line-height: 28upx;
+
+					.user_type {
 						font-size: 20upx;
 						font-family: PingFangSC-Regular;
 						font-weight: 400;
 						color: rgba(255, 230, 190, 1);
 					}
-					&:nth-of-type(2) {
-						width: 200upx;
-						font-size: 46upx;
-						font-family: DINAlternate-Bold;
-						font-weight: bold;
-						color: rgba(255, 255, 255, 1);
-						line-height: 54upx;
-					}
 				}
 			}
-			.pay_btn {
-				width: 116upx;
-				line-height: 46upx;
-				background: rgba(255, 255, 255, 1);
-				border-radius: 24upx;
-				font-size: 28upx;
-				font-family: PingFangSC-Regular;
-				font-weight: 400;
-				color: rgba(173, 137, 83, 1);
-				cursor: pointer;
+
+			.user_money {
+				display: flex;
+				align-items: flex-end;
+				justify-content: space-between;
+
+				.money_info {
+					display: flex;
+					flex-direction: column;
+					text-align: left;
+					margin-top: 40upx;
+
+					text {
+						&:nth-of-type(1) {
+							width: 100upx;
+							line-height: 28upx;
+							font-size: 20upx;
+							font-family: PingFangSC-Regular;
+							font-weight: 400;
+							color: rgba(255, 230, 190, 1);
+						}
+
+						&:nth-of-type(2) {
+							width: 200upx;
+							font-size: 46upx;
+							font-family: DINAlternate-Bold;
+							font-weight: bold;
+							color: rgba(255, 255, 255, 1);
+							line-height: 54upx;
+						}
+					}
+				}
+
+				.pay_btn {
+					width: 116upx;
+					line-height: 46upx;
+					background: rgba(255, 255, 255, 1);
+					border-radius: 24upx;
+					font-size: 28upx;
+					font-family: PingFangSC-Regular;
+					font-weight: 400;
+					color: rgba(173, 137, 83, 1);
+					cursor: pointer;
+				}
 			}
 		}
-	}
-	.class_list {
-		display: flex;
-		align-items: center;
-		justify-content: space-around;
-		margin-top: 20upx;
-		view {
+
+		.class_list {
 			display: flex;
-			flex-direction: column;
 			align-items: center;
-			image {
-				width: 68upx;
-				height: 68upx;
-				border-radius: 4upx;
+			justify-content: space-around;
+			margin-top: 20upx;
+
+			view {
+				display: flex;
+				flex-direction: column;
+				align-items: center;
+
+				image {
+					width: 68upx;
+					height: 68upx;
+					border-radius: 4upx;
+				}
+
+				text {
+					margin-top: 20upx;
+					font-size: 28upx;
+					font-family: PingFangSC-Medium;
+					font-weight: 600;
+					color: rgba(26, 26, 26, 1);
+					line-height: 28upx;
+				}
 			}
-			text {
-				margin-top: 20upx;
-				font-size: 28upx;
-				font-family: PingFangSC-Medium;
-				font-weight: 600;
-				color: rgba(26, 26, 26, 1);
-				line-height: 28upx;
+		}
+
+		.user_meun {
+			padding: 0 15upx;
+
+			view {
+				display: flex;
+				align-items: center;
+				justify-content: space-between;
+				line-height: 104upx;
+				border-bottom: 2upx solid #ddd;
+
+				:last-child {
+					border-bottom: 0;
+				}
+
+				text {
+					font-size: 32upx;
+					font-family: PingFangSC-Regular;
+					font-weight: 400;
+					color: rgba(51, 51, 51, 1);
+				}
+
+				image {
+					width: 44upx;
+					height: 44upx;
+				}
 			}
 		}
 	}
-	.user_meun {
-		padding: 0 15upx;
-		view {
-			display: flex;
-			align-items: center;
-			justify-content: space-between;
-			line-height: 104upx;
-			border-bottom: 2upx solid #ddd;
-			:last-child {
-				border-bottom: 0;
-			}
-			text {
-				font-size: 32upx;
-				font-family: PingFangSC-Regular;
-				font-weight: 400;
-				color: rgba(51, 51, 51, 1);
-			}
-			image {
-				width: 44upx;
-				height: 44upx;
-			}
-		}
-	}
-}
 </style>
