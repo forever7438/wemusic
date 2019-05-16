da11da77b3aeb386635751265cc5d97ed336d6f7
forget71 <743867239@qq.com>
2019-05-16 21:48:16 +0800

    bug修复

--- components/item/financeList.vue
+++ components/item/financeList.vue
@@ -7,8 +7,11 @@
 				<view class="finance_money">
 					<text>${{ item.money || 0.0 }}</text>
 					<text>{{ $t('index').money }}</text>
+					<text class="fun-tag1" v-if="item.status =='1'"></text>
+					<text class="fun-tag2" v-if="item.status =='2'"></text>
+					<text class="fun-tag3" v-if="item.status =='3'"></text>
+					<text class="fun-tag4" v-if="item.status =='4'"></text>
 				</view>
-				<text class="fun-tag"></text>
 			</view>
 			<view class="finance_option">
 				<view class="total_class">
@@ -19,8 +22,8 @@
 					<text>{{ item.student_num || 0 }}</text>
 					<text>{{ $t('index').Students }}</text>
 				</view>
-				<button hover-class="btn-hover" @tap="addFinance(item.id)">{{ $t('index').Apply }}</button>
-				<button hover-class="btn-hover" @tap="viewFinance(item.id)">{{ $t('index').See }}</button>
+				<button hover-class="btn-hover" v-if="item.status =='1'" @tap="addFinance(item.id)">{{ $t('index').Apply }}</button>
+				<button hover-class="btn-hover" v-else @tap="viewFinance(item.id)">{{ $t('index').See }}</button>
 			</view>
 		</view>
 		<noContent v-else title="暂无信息"></noContent>
@@ -28,157 +31,180 @@
 </template>
 
 <script>
-import { getDates } from '../../common/common.js';
-import noContent from '../noContent.vue';
-export default {
-	props: {
-		financeList: Array
-	},
-	components: {
-		noContent
-	},
-	filters: {
-		formateTime: time => {
-			return getDates(time);
+	import {
+		getDates
+	} from '../../common/common.js';
+	import noContent from '../noContent.vue';
+	export default {
+		props: {
+			financeList: Array
 		},
-		getDateYear: time => {
-			return new Date(time).getFullYear();
-		}
-	},
-	methods: {
-		//提交申请
-		addFinance(financeId) {
-			this.ajax({
-				url: 'teacherclass/finance_update',
-				data: {
-					finance_id: financeId
-				},
-				success: res => {
-					if (res.data.body === 'success') {
-						uni.showToast({
-							title: '您的申请已提交,请耐心等待',
-							icon: 'none'
-						});
-					} else {
-						uni.showToast({
-							title: res.data.msg,
-							icon: 'none'
-						});
+		components: {
+			noContent
+		},
+		filters: {
+			formateTime: time => {
+				return getDates(time);
+			},
+			getDateYear: time => {
+				return new Date(time).getFullYear();
+			}
+		},
+		methods: {
+			//提交申请
+			addFinance(financeId) {
+				this.ajax({
+					url: 'teacherclass/finance_update',
+					data: {
+						finance_id: financeId
+					},
+					success: res => {
+						if (res.data.body === 'success') {
+							uni.showToast({
+								title: '您的申请已提交,请耐心等待',
+								icon: 'none'
+							});
+						} else {
+							uni.showToast({
+								title: res.data.msg,
+								icon: 'none'
+							});
+						}
 					}
-				}
-			});
-		},
-		//查看工资单
-		viewFinance(financeId) {
-			uni.navigateTo({
-				url: '/pages/wagesDetail/wagesDetail?financeId=' + financeId
-			});
+				});
+			},
+			//查看工资单
+			viewFinance(financeId) {
+				uni.navigateTo({
+					url: '/pages/wagesDetail/wagesDetail?financeId=' + financeId
+				});
+			}
 		}
-	}
-};
+	};
 </script>
 
 <style lang="less">
-@import '../../common/global.less';
+	@import '../../common/global.less';
 
-.finance_list {
-	.finance_list_item {
-		padding: 0 30upx 10upx;
-		border-bottom: 2upx solid #ddd;
+	.finance_list {
+		.finance_list_item {
+			padding: 0 30upx 10upx;
+			border-bottom: 2upx solid #ddd;
 
-		.finance_year {
-			display: inline-block;
-			margin: 20upx 0;
-			font-size: 48upx;
-			font-family: PingFangSC-Medium;
-			font-weight: 500;
-			color: rgba(51, 51, 51, 1);
-		}
-
-		.finance_content {
-			position: relative;
-			display: flex;
-			align-items: end;
-			flex-direction: column;
-
-			.finance_time {
-				font-size: 24upx;
-				font-family: PingFangSC-Regular;
-				font-weight: 400;
-				color: rgba(51, 51, 51, 1);
-			}
-
-			.finance_money {
+			.finance_year {
+				display: inline-block;
 				margin: 20upx 0;
-				display: flex;
-				align-items: center;
-
-				text {
-					&:nth-of-type(1) {
-						margin-right: 30upx;
-						font-size: 48upx;
-						font-family: DINAlternate-Bold;
-						font-weight: bold;
-						color: rgba(51, 51, 51, 1);
-					}
-
-					&:nth-of-type(2) {
-						font-size: 24upx;
-						font-family: PingFangSC-Regular;
-						font-weight: 400;
-						color: rgba(51, 51, 51, 1);
-					}
-				}
-			}
-
-			.fun-tag {
-				position: absolute !important;
-				top: -40upx;
-				right: -82upx;
-				.global-fun-tag(80upx, #ff4040, '未发放');
-			}
-		}
-
-		.finance_option {
-			display: flex;
-			align-items: center;
-			justify-content: space-between;
-
-			.total_class,
-			.total_student {
-				display: flex;
-				align-items: center;
-
-				text {
-					&:nth-of-type(1) {
-						margin-right: 30upx;
-						font-size: 48upx;
-						font-family: DINAlternate-Bold;
-						font-weight: bold;
-						color: rgba(51, 51, 51, 1);
-					}
-
-					&:nth-of-type(2) {
-						font-size: 24upx;
-						font-family: PingFangSC-Regular;
-						font-weight: 400;
-						color: rgba(51, 51, 51, 1);
-					}
-				}
-			}
-
-			button {
-				width: 128upx;
-				line-height: 44upx;
-				margin-right: 0;
-				font-size: 28upx;
-				font-family: PingFangSC-Regular;
-				font-weight: 400;
+				font-size: 48upx;
+				font-family: PingFangSC-Medium;
+				font-weight: 500;
 				color: rgba(51, 51, 51, 1);
-				background: rgba(250, 212, 42, 1);
-				border-radius: 6upx;
+			}
+
+			.finance_content {
+				position: relative;
+				display: flex;
+				align-items: end;
+				flex-direction: column;
+
+				.finance_time {
+					font-size: 24upx;
+					font-family: PingFangSC-Regular;
+					font-weight: 400;
+					color: rgba(51, 51, 51, 1);
+				}
+
+				.finance_money {
+					margin: 20upx 0;
+					display: flex;
+					align-items: center;
+
+					.fun-tag1 {
+						position: absolute !important;
+						top: -40upx;
+						right: -20upx;
+						.global-fun-tag(80upx, #FABE2A, '进行中');
+					}
+
+					.fun-tag2 {
+						position: absolute !important;
+						top: -40upx;
+						right: -20upx;
+						.global-fun-tag(80upx, #93E517, '已结款');
+					}
+
+					.fun-tag3 {
+						position: absolute !important;
+						top: -40upx;
+						right: -20upx;
+						.global-fun-tag(80upx, #999999, '已拒绝');
+					}
+
+					.fun-tag4 {
+						position: absolute !important;
+						top: -40upx;
+						right: -20upx;
+						.global-fun-tag(80upx, #ff4040, '未结款');
+					}
+
+					text {
+						&:nth-of-type(1) {
+							margin-right: 30upx;
+							font-size: 48upx;
+							font-family: DINAlternate-Bold;
+							font-weight: bold;
+							color: rgba(51, 51, 51, 1);
+						}
+
+						&:nth-of-type(2) {
+							font-size: 24upx;
+							font-family: PingFangSC-Regular;
+							font-weight: 400;
+							color: rgba(51, 51, 51, 1);
+						}
+					}
+				}
+			}
+
+			.finance_option {
+				display: flex;
+				align-items: center;
+				justify-content: space-between;
+
+				.total_class,
+				.total_student {
+					display: flex;
+					align-items: center;
+
+					text {
+						&:nth-of-type(1) {
+							margin-right: 30upx;
+							font-size: 48upx;
+							font-family: DINAlternate-Bold;
+							font-weight: bold;
+							color: rgba(51, 51, 51, 1);
+						}
+
+						&:nth-of-type(2) {
+							font-size: 24upx;
+							font-family: PingFangSC-Regular;
+							font-weight: 400;
+							color: rgba(51, 51, 51, 1);
+						}
+					}
+				}
+
+				button {
+					width: 128upx;
+					line-height: 44upx;
+					margin-right: 30upx;
+					font-size: 28upx;
+					font-family: PingFangSC-Regular;
+					font-weight: 400;
+					color: rgba(51, 51, 51, 1);
+					background: rgba(250, 212, 42, 1);
+					border-radius: 6upx;
+				}
 			}
 		}
 	}
-}
 </style>
--- components/item/friendsList.vue
+++ components/item/friendsList.vue
@@ -3,84 +3,78 @@
 		<view class="fridend_content" v-for="(item, index) in friendsList" :key="index">
 			<friendHead :itemHead="item"></friendHead>
 			<friendContent :type="true" :content="item.body" :listId="item.id"></friendContent>
-			<view class="img_lists" v-if="Array.isArray(item.video)">
-				<image class="image" @tap="previewImage(index, item.video)" :src="val || userImage" v-for="(val, index) in item.video" :key="index"></image>
+			<view class="img_lists" v-if="Array.isArray(item.video)" @tap="previewImage(index, item.video)">
+				<image class="image" :src="val || userImage" v-for="(val, index) in item.video" :key="index"></image>
 			</view>
 			<view class="img_lists" v-else><video class="video" id="video" :src="item.video" controls></video></view>
-			<friendOperation
-				:message="item.message_num"
-				:is_forward="item.is_forward"
-				:is_praise="item.is_praise"
-				:praise="item.praise_num"
-				:forward="item.forward_num"
-				:listId="item.id"
-				:index="index"
-				@refreshData="getFriendList"
-				@changeStatus="changeStatus"
-			></friendOperation>
+			<friendOperation :message="item.message_num" :is_forward="item.is_forward" :is_praise="item.is_praise" :praise="item.praise_num"
+			 :forward="item.forward_num" :listId="item.id" :index="index" @refreshData="getFriendList" @changeStatus="changeStatus"></friendOperation>
 		</view>
 	</view>
 </template>
 
 <script>
-import friendHead from '../friendsAssembly/friendHead.vue';
-import friendContent from '../friendsAssembly/friendContent.vue';
-import friendOperation from '../friendsAssembly/friendOperation.vue';
-export default {
-	components: {
-		friendHead,
-		friendContent,
-		friendOperation
-	},
-	props: {
-		friendsList: Array
-	},
-	data() {
-		return {
-			userImage: '../../static/img/lf.jpg'
-		};
-	},
-	methods: {
-		//获取朋友圈列表
-		getFriendList() {
-			getCurrentPages().map(res => {
-				if (res.route == 'pages/circleFriends/circleFriends' || res.route == 'pages/myFriend/myFriend') {
-					res.getFriendList(0);
+	import friendHead from '../friendsAssembly/friendHead.vue';
+	import friendContent from '../friendsAssembly/friendContent.vue';
+	import friendOperation from '../friendsAssembly/friendOperation.vue';
+	export default {
+		components: {
+			friendHead,
+			friendContent,
+			friendOperation
+		},
+		props: {
+			friendsList: Array
+		},
+		data() {
+			return {
+				userImage: '../../static/img/lf.jpg'
+			};
+		},
+		methods: {
+			//获取朋友圈列表
+			getFriendList() {
+				getCurrentPages().map(res => {
+					if (res.route == 'pages/circleFriends/circleFriends' || res.route == 'pages/myFriend/myFriend') {
+						res.getFriendList(0);
+					}
+				});
+			},
+			changeStatus(data) {
+				this.$emit('forward_praise', data);
+			},
+			previewImage(index, video) {
+				if (Array.isArray(video)) {
+					uni.previewImage({
+						current: video[index],
+						indicator: 'number',
+						loop: 'true',
+						urls: video
+					});
 				}
-			});
-		},
-		changeStatus(data) {
-			this.$emit('forward_praise', data);
-		},
-		previewImage(index, video) {
-			uni.previewImage({
-				current: video[index],
-				indicator: 'number',
-				loop: 'true',
-				urls: video
-			});
+
+			}
 		}
-	}
-};
+	};
 </script>
 
 <style lang="less">
-.friend_list {
-	.fridend_content {
-		margin-bottom: 20upx;
+	.friend_list {
+		.fridend_content {
+			margin-bottom: 20upx;
 
-		.img_lists {
-			.image {
-				width: 210upx;
-				height: 210upx;
-				margin: 0 20upx 20upx 0;
-				border-radius: 12upx;
-			}
+			.img_lists {
+				.image {
+					width: 210upx;
+					height: 210upx;
+					margin: 0 20upx 20upx 0;
+					border-radius: 12upx;
+				}
 
-			.video {
-				width: 100%;
+				.video {
+					width: 100%;
+				}
 			}
 		}
 	}
-}
 </style>
